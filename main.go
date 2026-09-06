d6f2101c63227cbd589be660dece7aec0fe21dda
eunwoo song <presentsong@naver.com>
2026-09-07 03:42:14 +0900

    feat: add HTTP agent invocation endpoint
    Signed-off-by: eunwoo song <presentsong@naver.com>

--- main.go
+++ main.go
@@ -1,9 +1,21 @@
 package main
 
 import (
+	api "agent-harness/api"
 	cli "agent-harness/cli"
+	"context"
+	"flag"
+	"log"
 )
 
 func main() {
+	httpAddress := flag.String("http", "", "serve the HTTP API on this address instead of starting the CLI")
+	flag.Parse()
+	if *httpAddress != "" {
+		if err := api.ListenAndServe(context.Background(), *httpAddress); err != nil {
+			log.Fatal(err)
+		}
+		return
+	}
 	cli.StartCli()
 }
